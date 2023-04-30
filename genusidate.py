898e9868bebabb8468eba5ff4bba81a1da6b31e8
zoobereq <simon@zuberek.net>
2023-04-30 19:44:50 -0400

    adds CLI

--- genusidate.py
+++ genusidate.py
@@ -1,59 +1,32 @@
 #!/usr/bin/env python
 
 import argparse
+
 import spacy
-import wn
-import googletrans
-from nltk.corpus import wordnet as wn
-# from odenet import *
-
-
-#from google_trans_new import google_translator
 from googletrans import Translator
+
+from hypernyms import taxonomy
+
 translator = Translator()
+nlp = spacy.load(
+    "de_core_news_lg"
+)  # lemmatizer doesn't work as well when trained on smaller corpora
 
 
-
-#wn.download('odenet')
-#wn.download('oewn')
-#de = wn.Wordnet('odenet')
-#en = wn.Wordnet('oewn')
-nlp = spacy.load("de_core_news_lg") # lemmatizer doesn't work as well on smaller corpora
-
-nouns = "Katze"
-
-def main() -> None:
+def main(args: argparse.Namespace) -> None:
+    nouns = args.input
     doc = nlp(nouns)
     for noun in doc:
-        lemmatized = noun.lemma_
-        translation = translator.translate(lemmatized, src='de', dest='en')
-        translation.text
-        target_word = wn.synset(translation.text)[0]
-        print(target_word.hyperhyms())
-        
-        #word = de.senses(lemmatized)[0]
-        #en_senses = word.senses()[0].translate(lexicon='oewn')
-        #print(word)
-        #print([s.word().lemma() for s in en_senses])
-
-"""
-word = de.synsets(lemmatized, pos='n')[0]
-hypernyms = word.hypernyms()
-for hypernym in hypernyms:
-    print(hypernym.words())
-"""
-        
-
-
-        
-
-
-# def main(args: argparse.Namespace) -> None:
-#     pass
+        lemmatized = noun.lemma_  # run it throug the German lemmatizer
+        translation = translator.translate(
+            lemmatized, src="de", dest="en"
+        )  # translate into EN
+        translated = translation.text.casefold()
+        hypernyms = taxonomy(translated)
+        print(hypernyms)
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description=__doc__)
-    # parser.add_argument("--input", help="provide a German noun")
-    # main(parser.parse_args())
-    main()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--input", help="provide a German noun")
+    main(parser.parse_args())
