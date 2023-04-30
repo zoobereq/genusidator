212439923499f1f9b6ba59886cb32d4df748b904
zoobereq <simon@zuberek.net>
2023-04-30 12:06:00 -0400

    generates the hypernym taxonomy

---
+++ hypernyms.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""A function that generates a noun-restricted taxonomy of hypernyms for input string"""
+
+from nltk.corpus import wordnet as wn
+
+
+def taxonomy(word: str) -> list:
+    hypernym_list = []
+    target = wn.synsets(word, pos=wn.NOUN)[
+        0
+    ]  # generate the meaning (assume the first synset is the default)
+    hyper = lambda s: s.hypernyms()  # function to iterate up the hypernym taxonomy
+    if (
+        list(target.closure(hyper, depth=1)) == target.hypernyms()
+    ):  # validate closure overthe hypernym hierarchy
+        hypernyms = list(target.closure(hyper))  # generate a list of hypernym synsets
+        for synset in hypernyms:
+            for lemma in synset.lemmas():
+                hypernym_list.append(lemma.name().replace("_", " "))
+    return hypernym_list
