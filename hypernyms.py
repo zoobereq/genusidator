fb87b7860dfac1d4efe9829d7bb64849581f8ab6
zoobereq <simon@zuberek.net>
2023-04-30 20:42:04 -0400

    removes duplicated semantic classes from the output

--- hypernyms.py
+++ hypernyms.py
@@ -5,21 +5,28 @@ from nltk.corpus import wordnet as wn
 
 
 def taxonomy(word: str) -> list:
-    hypernym_list = []
-    if wn.synsets(word, pos=wn.NOUN):
-        target = wn.synsets(word, pos=wn.NOUN)[
-            0
-        ]  # generate the meaning (assume the first synset is the default)
-        hyper = lambda s: s.hypernyms()  # function to iterate up the hypernym taxonomy
-        if (
-            list(target.closure(hyper, depth=1)) == target.hypernyms()
-        ):  # validate closure overthe hypernym hierarchy
-            hypernyms = list(
-                target.closure(hyper)
-            )  # generate a list of hypernym synsets
-            for synset in hypernyms:
-                for lemma in synset.lemmas():
-                    hypernym_list.append(lemma.name().replace("_", " "))
-        return hypernym_list
+    all_hypernyms = []
+    if wn.synsets(
+        word, pos=wn.NOUN
+    ):  # checks if there are any synsets to begin with. Proper nouns don't have synsets.
+        synsets = wn.synsets(word, pos=wn.NOUN)
+        for synset in synsets:
+            hypernym_list = []
+            hyper = (
+                lambda s: s.hypernyms()
+            )  # function to iterate up the hypernym taxonomy
+            if (
+                list(synset.closure(hyper, depth=1)) == synset.hypernyms()
+            ):  # validats closure over the hypernym hierarchy
+                hypernyms = list(
+                    synset.closure(hyper)
+                )  # generates a list of hypernym synsets
+                for synset in hypernyms:
+                    for lemma in synset.lemmas():
+                        hypernym_list.append(lemma.name().replace("_", " "))
+            all_hypernyms.append(hypernym_list)
+        return list(
+            set([item for sublist in all_hypernyms for item in sublist])
+        )  # flattens the list and removes duplicate categories
     else:
         return "There are no synsets available for this word."
