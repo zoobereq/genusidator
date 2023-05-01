fc4796d3ccf9b7c09176511b7be2cb6cdbe13689
zoobereq <simon@zuberek.net>
2023-04-30 20:25:01 -0400

    formats the syntax

--- hypernyms.py
+++ hypernyms.py
@@ -6,15 +6,20 @@ from nltk.corpus import wordnet as wn
 
 def taxonomy(word: str) -> list:
     hypernym_list = []
-    target = wn.synsets(word, pos=wn.NOUN)[
-        0
-    ]  # generate the meaning (assume the first synset is the default)
-    hyper = lambda s: s.hypernyms()  # function to iterate up the hypernym taxonomy
-    if (
-        list(target.closure(hyper, depth=1)) == target.hypernyms()
-    ):  # validate closure overthe hypernym hierarchy
-        hypernyms = list(target.closure(hyper))  # generate a list of hypernym synsets
-        for synset in hypernyms:
-            for lemma in synset.lemmas():
-                hypernym_list.append(lemma.name().replace("_", " "))
-    return hypernym_list
+    if wn.synsets(word, pos=wn.NOUN):
+        target = wn.synsets(word, pos=wn.NOUN)[
+            0
+        ]  # generate the meaning (assume the first synset is the default)
+        hyper = lambda s: s.hypernyms()  # function to iterate up the hypernym taxonomy
+        if (
+            list(target.closure(hyper, depth=1)) == target.hypernyms()
+        ):  # validate closure overthe hypernym hierarchy
+            hypernyms = list(
+                target.closure(hyper)
+            )  # generate a list of hypernym synsets
+            for synset in hypernyms:
+                for lemma in synset.lemmas():
+                    hypernym_list.append(lemma.name().replace("_", " "))
+        return hypernym_list
+    else:
+        return "There are no synsets available for this word."
